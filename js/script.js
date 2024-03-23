da7ab0683dbea9dd786be58529e2d99efb25c500
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-03-23 23:35:43 +0100

    Añadido fetch para traducir texto

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-$(document).ready(function () {
-
-});
+window.onload = function () {
+    const translateButton = document.getElementById("translate-button");
+    const translateText = document.getElementById('first-translate-input').value;
+    translateButton.onclick = function () {
+        fetch('https://api.mymemory.translated.net/get?q=' + translateText + '&langpair=en|fr')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.responseData.translatedText)
+                document.getElementById("second-translate-input").value = data.responseData.translatedText;
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    };
+};
