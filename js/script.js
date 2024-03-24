a5c6cdea4e22d5312ca078e1a409f2fda5d7ef77
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-03-24 21:49:05 +0100

    Contador de caracteres y cambios en el diseño.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 window.onload = function () {
     const translateButton = document.getElementById("translate-button");
-    const translateText = document.getElementById('first-translate-input').value;
+    const translation = document.getElementById('first-translate-input');
+    const translationCount = document.getElementById('count-translation-textarea');
+    let translationText = "";
+    translationText = translation.value;
+    translationCount.innerHTML = translationText.length;
+
     translateButton.onclick = function () {
-        fetch('https://api.mymemory.translated.net/get?q=' + translateText + '&langpair=en|fr')
+        translationText = translation.value;
+        fetch('https://api.mymemory.translated.net/get?q=' + translationText + '&langpair=en|fr')
             .then(response => response.json())
             .then(data => {
                 console.log(data.responseData.translatedText)
@@ -12,4 +18,8 @@ window.onload = function () {
                 console.log(error)
             })
     };
+    translation.addEventListener("keyup", (event) => {
+        translationText = translation.value;
+        translationCount.innerHTML = translationText.length;
+    });
 };
